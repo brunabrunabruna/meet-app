53b3eccb500b300e94a6c2d6fc0a3a166a16479e
brunabrunabruna <brunaandreis@hotmail.com>
2023-10-16 15:41:09 +0200

    added testing and development for first 3 features

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,8 +4,22 @@ import { render } from "@testing-library/react";
 import App from "../App";
 
 describe("<App /> component", () => {
+  let AppDOM;
+
+  //before each test is run, this funtion runs ( makes a clean slate with the <App/>)
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  });
+
   test("renders list of events", () => {
-    const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
+
+  test("render citySearch", () => {
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+  });
+
+  test("render number of events component", () => {
+    expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
   });
 });
